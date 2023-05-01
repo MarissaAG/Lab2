d147a6b1e7fefee7f0b84678cb00b20526705d16
MarissaAG <747121@student.dusd.net>
2023-05-01 12:19:42 -0700

    player controller

--- Lab2/Assets/Scripts/PlayerController.cs
+++ Lab2/Assets/Scripts/PlayerController.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInput;
-    public float speed = 20.0f;
-    public float xRange = 10;
-    public GameObject
+    public 
+    public float speed = 10.0f;
+   
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * verticalMove + transform.right * horizontalMove;
+        characterController.Move(speed * Time.deltaTime * move);
     }
 }
